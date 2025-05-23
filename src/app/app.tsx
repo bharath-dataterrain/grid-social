f584779762f69b4d8344cc402fa6cb6cd8714ca5
bharath-dt <bharath@dataterrain.com>
2025-05-23 15:54:03 +0530

    first commit

---
+++ src/app/app.tsx
@@ -0,0 +1,15 @@
+
+import { Button } from "@lwj/shared-components"
+
+export function App() {
+  return (
+    <div style={{ backgroundColor: "whitesmoke", display: 'flex', flexDirection: "column", justifyContent: 'center', alignItems: 'center' }}>
+      <h3>Library</h3>
+      <br />
+      <Button></Button>
+      <br />
+    </div>
+  );
+}
+
+export default App;
